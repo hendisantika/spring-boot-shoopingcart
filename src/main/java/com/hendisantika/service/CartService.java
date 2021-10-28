85b7dd7e892f24915dd13838cd4468f70d2267ae
Hendi Santika <hendisantika@gmail.com>
2021-10-29 06:37:31 +0700

    #30 - Add CartService

---
+++ src/main/java/com/hendisantika/service/CartService.java
@@ -0,0 +1,25 @@
+package com.hendisantika.service;
+
+import com.hendisantika.repository.CartRepository;
+import com.hendisantika.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-shoopingcart
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/10/21
+ * Time: 06.37
+ */
+@Service
+public class CartService {
+
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    ItemRepository itemRepository;
+}
