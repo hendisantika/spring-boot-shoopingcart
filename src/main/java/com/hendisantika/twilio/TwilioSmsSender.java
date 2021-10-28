baf7f6993b004f968534051696378c54297534d4
Hendi Santika <hendisantika@gmail.com>
2021-10-29 06:50:13 +0700

    #54 - Add TwilioSmsSender

---
+++ src/main/java/com/hendisantika/twilio/TwilioSmsSender.java
@@ -0,0 +1,26 @@
+package com.hendisantika.twilio;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-shoopingcart
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/10/21
+ * Time: 06.49
+ */
+public class TwilioSmsSender {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TwilioSmsSender.class);
+
+    private final TwilioConfiguration twilioConfiguration;
+
+    @Autowired
+    public TwilioSmsSender(TwilioConfiguration twilioConfiguration) {
+        this.twilioConfiguration = twilioConfiguration;
+    }
+
+}
