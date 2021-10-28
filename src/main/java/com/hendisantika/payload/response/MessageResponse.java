1cf2e44dfab4923e0c6fc2011a668b37b1489f92
Hendi Santika <hendisantika@gmail.com>
2021-10-29 06:36:57 +0700

    #29 - Add MessageResponse Payload

---
+++ src/main/java/com/hendisantika/payload/response/MessageResponse.java
@@ -0,0 +1,19 @@
+package com.hendisantika.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-shoopingcart
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/10/21
+ * Time: 06.36
+ */
+@Data
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+}
