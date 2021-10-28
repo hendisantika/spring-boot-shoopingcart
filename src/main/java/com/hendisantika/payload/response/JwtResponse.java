d81bb01a7564fb3a49037424bfdd031cfe35fe50
Hendi Santika <hendisantika@gmail.com>
2021-10-29 06:36:21 +0700

    #28 - Add JwtResponse Payload

---
+++ src/main/java/com/hendisantika/payload/response/JwtResponse.java
@@ -0,0 +1,24 @@
+package com.hendisantika.payload.response;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-shoopingcart
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/10/21
+ * Time: 06.36
+ */
+@Data
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String username;
+    private String email;
+    private List<String> roles;
+}
