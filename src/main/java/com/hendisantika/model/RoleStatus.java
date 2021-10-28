1ad8fcafce981805588e70c6fcf2c57557a80bde
Hendi Santika <hendisantika@gmail.com>
2021-10-29 06:22:41 +0700

    #4 - Add RoleStatus Enum

---
+++ src/main/java/com/hendisantika/model/RoleStatus.java
@@ -0,0 +1,16 @@
+package com.hendisantika.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-shoopingcart
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/10/21
+ * Time: 06.22
+ */
+public enum RoleStatus {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
