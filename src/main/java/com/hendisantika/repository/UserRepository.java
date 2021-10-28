d13082fb3e0fb969ca8dedc066cde5fcc09fca63
Hendi Santika <hendisantika@gmail.com>
2021-10-29 06:31:55 +0700

    #22 - Add more services in UserRepository

--- src/main/java/com/hendisantika/repository/UserRepository.java
+++ src/main/java/com/hendisantika/repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.hendisantika.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-shoopingcart
@@ -15,4 +17,9 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
