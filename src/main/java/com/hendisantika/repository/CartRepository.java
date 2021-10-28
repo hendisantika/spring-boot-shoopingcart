2af373e269137660737722c88a25f880e188e4ee
Hendi Santika <hendisantika@gmail.com>
2021-10-29 06:27:39 +0700

    #10 - Add getCartDetailsByCustId in CartRepository

--- src/main/java/com/hendisantika/repository/CartRepository.java
+++ src/main/java/com/hendisantika/repository/CartRepository.java
@@ -2,7 +2,11 @@ package com.hendisantika.repository;
 
 import com.hendisantika.model.CartRecords;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,4 +20,7 @@ import org.springframework.stereotype.Repository;
 @Transactional
 @Repository
 public interface CartRepository extends JpaRepository<CartRecords, Integer> {
+    @Query(value = "SELECT * from cart_item c WHERE c.user_id = ?1 AND c.status = '" + "P" + "'", nativeQuery = true)
+    List<CartRecords> getCartDetailsByCustId(int custId);
+
 }
