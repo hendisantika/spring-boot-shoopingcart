24f1d542c5661cba2c1cd5a47338141907c3a5c6
Hendi Santika <hendisantika@gmail.com>
2021-10-29 06:58:46 +0700

    #59 - Add SmsController

---
+++ src/main/java/com/hendisantika/twilio/SmsController.java
@@ -0,0 +1,28 @@
+package com.hendisantika.twilio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-shoopingcart
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/10/21
+ * Time: 06.58
+ */
+@RestController
+@CrossOrigin("*")
+@RequestMapping("api/v1/sms")
+public class SmsController {
+
+    private final SmsService smsService;
+
+    @Autowired
+    public SmsController(SmsService smsService) {
+        this.smsService = smsService;
+    }
+}
