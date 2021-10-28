507f1bdb4ba6e01e95eb444f98a4379a40aba7ac
Hendi Santika <hendisantika@gmail.com>
2021-10-29 06:27:52 +0700

    #11 - Add getCartDetailsByCustIdAndId in CartRepository

--- src/main/java/com/hendisantika/repository/CartRepository.java
+++ src/main/java/com/hendisantika/repository/CartRepository.java
@@ -23,4 +23,7 @@ public interface CartRepository extends JpaRepository<CartRecords, Integer> {
     @Query(value = "SELECT * from cart_item c WHERE c.user_id = ?1 AND c.status = '" + "P" + "'", nativeQuery = true)
     List<CartRecords> getCartDetailsByCustId(int custId);
 
+    @Query(value = "SELECT * from cart_item c WHERE c.user_id = ?1 AND c.item_id = ?2", nativeQuery = true)
+    List<CartRecords> getCartDetailsByCustIdAndId(int custId, int id);
+
 }
