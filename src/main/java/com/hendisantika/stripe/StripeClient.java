9c05965d1ef9b312ed3a877f898241e82e42bd4a
Hendi Santika <hendisantika@gmail.com>
2021-10-29 06:44:47 +0700

    #48 - Add createCustomer in StripeClient

--- src/main/java/com/hendisantika/stripe/StripeClient.java
+++ src/main/java/com/hendisantika/stripe/StripeClient.java
@@ -3,9 +3,13 @@ package com.hendisantika.stripe;
 import com.hendisantika.repository.CartRepository;
 import com.hendisantika.service.ItemService;
 import com.stripe.Stripe;
+import com.stripe.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -32,4 +36,12 @@ public class StripeClient {
     StripeClient() {
         Stripe.apiKey = STRIPE_API_KEY;
     }
+
+    public Customer createCustomer(String token, String email) throws Exception {
+        Map<String, Object> customerParams = new HashMap<String, Object>();
+        customerParams.put("email", email);
+        customerParams.put("source", token);
+        return Customer.create(customerParams);
+    }
+
 }
