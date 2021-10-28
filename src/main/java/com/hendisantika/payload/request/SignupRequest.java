d74c2a8aeab28e717265295b369c742b53ce7159
Hendi Santika <hendisantika@gmail.com>
2021-10-29 06:34:34 +0700

    #25 - Add SignupRequest

---
+++ src/main/java/com/hendisantika/payload/request/SignupRequest.java
@@ -0,0 +1,35 @@
+package com.hendisantika.payload.request;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-shoopingcart
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/10/21
+ * Time: 06.34
+ */
+@Data
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    private Set<String> role;
+
+    @NotBlank
+    private String password;
+
+}
