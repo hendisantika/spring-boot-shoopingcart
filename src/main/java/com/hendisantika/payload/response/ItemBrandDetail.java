87e72b212bc7fa771f80015bb0a8a87c04f4db18
Hendi Santika <hendisantika@gmail.com>
2021-10-29 06:35:56 +0700

    #27 - Add ItemBrandDetail Payload

---
+++ src/main/java/com/hendisantika/payload/response/ItemBrandDetail.java
@@ -0,0 +1,19 @@
+package com.hendisantika.payload.response;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-shoopingcart
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/10/21
+ * Time: 06.35
+ */
+@Data
+public class ItemBrandDetail {
+    private String brand;
+    private int quantity;
+
+}
