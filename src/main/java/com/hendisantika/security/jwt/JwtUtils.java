3b591937b94385e123190cbb6e82fa93e0edec13
Hendi Santika <hendisantika@gmail.com>
2021-10-30 06:23:45 +0700

    #73 - Add generateJwtToken in JwtUtils

--- src/main/java/com/hendisantika/security/jwt/JwtUtils.java
+++ src/main/java/com/hendisantika/security/jwt/JwtUtils.java
@@ -1,9 +1,15 @@
 package com.hendisantika.security.jwt;
 
+import com.hendisantika.security.service.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +29,15 @@ public class JwtUtils {
 
     @Value("${hendisantika.app.jwtExpirationMs}")
     private int jwtExpirationMs;
+
+    public String generateJwtToken(Authentication authentication) {
+        UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject((userPrincipal.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
 }
