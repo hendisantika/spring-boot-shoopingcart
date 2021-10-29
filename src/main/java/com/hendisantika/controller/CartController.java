a8581e5302564582b139530cc163b7834d8dd732
Hendi Santika <hendisantika@gmail.com>
2021-10-30 06:32:55 +0700

    #86 - Add addToCart in CartController

--- src/main/java/com/hendisantika/controller/CartController.java
+++ src/main/java/com/hendisantika/controller/CartController.java
@@ -1,10 +1,16 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.model.CartRecords;
 import com.hendisantika.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +29,10 @@ public class CartController {
     @Autowired
     CartService cartService;
 
+    @PostMapping
+    @PreAuthorize("hasRole('USER')")
+    public HashMap<String, String> addToCart(@RequestBody CartRecords cart) throws Exception {
+        HashMap<String, String> response = cartService.addToCart(cart);
+        return response;
+    }
 }
