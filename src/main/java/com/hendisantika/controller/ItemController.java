95f75951ec4da44df3269489484fbae3d53ef877
Hendi Santika <hendisantika@gmail.com>
2021-10-30 06:35:48 +0700

    #95 - Add getItemById in ItemController

--- src/main/java/com/hendisantika/controller/ItemController.java
+++ src/main/java/com/hendisantika/controller/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +40,11 @@ public class ItemController {
     @GetMapping
     public List<Item> getAllItems() {
         return itemService.getAllItems();
+    }
+
+    @GetMapping("/{id}")
+    public Item getItemById(@PathVariable int id) {
+        return itemService.getItemById(id);
     }
 
 }
