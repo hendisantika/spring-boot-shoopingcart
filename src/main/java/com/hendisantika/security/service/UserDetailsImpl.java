8740462a7c2572842635b97d8ff06e96b17a82f4
Hendi Santika <hendisantika@gmail.com>
2021-10-30 06:21:13 +0700

    #69 - Add UserDetailsImpl in UserDetailsImpl

--- src/main/java/com/hendisantika/security/service/UserDetailsImpl.java
+++ src/main/java/com/hendisantika/security/service/UserDetailsImpl.java
@@ -1,10 +1,14 @@
 package com.hendisantika.security.service;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.hendisantika.model.User;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -38,4 +42,16 @@ public class UserDetailsImpl implements UserDetails {
         this.authorities = authorities;
     }
 
+    public static UserDetailsImpl build(User user) {
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toList());
+
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities);
+    }
 }
