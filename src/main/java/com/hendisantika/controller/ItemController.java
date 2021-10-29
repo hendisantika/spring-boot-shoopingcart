8fa4039e7db9b6dd2872f861e236d25cd0344a7f
Hendi Santika <hendisantika@gmail.com>
2021-10-30 06:36:42 +0700

    #98 - Add updateItem in ItemController

--- src/main/java/com/hendisantika/controller/ItemController.java
+++ src/main/java/com/hendisantika/controller/ItemController.java
@@ -4,14 +4,7 @@ import com.hendisantika.model.Item;
 import com.hendisantika.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -57,5 +50,11 @@ public class ItemController {
 //    @PreAuthorize("hasRole('MODERATOR')")
     public String deleteItemById(@PathVariable int id) {
         return itemService.deleteItemByID(id);
+    }
+
+    @PutMapping
+//    @PreAuthorize("hasRole('MODERATOR')")
+    public Item updateItem(@RequestBody Item item) {
+        return itemService.updateItemById(item);
     }
 }
