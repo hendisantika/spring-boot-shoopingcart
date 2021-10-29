d561bfe739436fabfdb89bcd317856f26f8bd5d9
Hendi Santika <hendisantika@gmail.com>
2021-10-30 06:20:36 +0700

    #67 - Add UserDetailsImpl

---
+++ src/main/java/com/hendisantika/security/service/UserDetailsImpl.java
@@ -0,0 +1,32 @@
+package com.hendisantika.security.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-shoopingcart
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 30/10/21
+ * Time: 06.20
+ */
+public class UserDetailsImpl implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+}
