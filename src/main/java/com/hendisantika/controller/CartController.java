54edd71702ae7e72331720ee6f7c4413b2c75d84
Hendi Santika <hendisantika@gmail.com>
2021-10-30 06:34:03 +0700

    #90 - Add updateItemInCart in CartController

--- src/main/java/com/hendisantika/controller/CartController.java
+++ src/main/java/com/hendisantika/controller/CartController.java
@@ -5,14 +5,7 @@ import com.hendisantika.payload.response.CartResponse;
 import com.hendisantika.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -58,4 +51,11 @@ public class CartController {
     public String deleteItemInCartById(@PathVariable int id) {
         return cartService.deleteItemByID(id);
     }
+
+    @PutMapping
+    @PreAuthorize("hasRole('USER')")
+    public CartRecords updateItemInCart(@RequestBody CartRecords cart) {
+        return cartService.updateItemById(cart);
+    }
+
 }
