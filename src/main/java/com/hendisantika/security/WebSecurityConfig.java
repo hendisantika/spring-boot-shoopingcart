0ae0a1e92899f15d5145412712f82fa95a968bae
Hendi Santika <hendisantika@gmail.com>
2021-10-30 06:26:33 +0700

    #79 - Add authenticationJwtTokenFilter in WebSecurityConfig

--- src/main/java/com/hendisantika/security/WebSecurityConfig.java
+++ src/main/java/com/hendisantika/security/WebSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.hendisantika.security;
 
+import com.hendisantika.security.jwt.AuthEntryPointJwt;
+import com.hendisantika.security.jwt.AuthTokenFilter;
 import com.hendisantika.security.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -28,4 +32,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
 }
