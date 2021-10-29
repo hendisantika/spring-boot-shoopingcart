508b55b94b7026e5e0f47f6b0f1bfb323a972f27
Hendi Santika <hendisantika@gmail.com>
2021-10-30 06:24:47 +0700

    #75 - Add AuthTokenFilter

---
+++ src/main/java/com/hendisantika/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,25 @@
+package com.hendisantika.security.jwt;
+
+import com.hendisantika.security.service.UserDetailsServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-shoopingcart
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 30/10/21
+ * Time: 06.24
+ */
+public class AuthTokenFilter extends OncePerRequestFilter {
+    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+    @Autowired
+    private JwtUtils jwtUtils;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+}
