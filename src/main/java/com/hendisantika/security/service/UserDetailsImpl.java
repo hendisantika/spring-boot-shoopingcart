ef048671535345448aa4c59626ef0d975e5199a4
Hendi Santika <hendisantika@gmail.com>
2021-10-30 06:20:53 +0700

    #68 - Add UserDetailsImpl in UserDetailsImpl

--- src/main/java/com/hendisantika/security/service/UserDetailsImpl.java
+++ src/main/java/com/hendisantika/security/service/UserDetailsImpl.java
@@ -18,15 +18,24 @@ import java.util.Collection;
 public class UserDetailsImpl implements UserDetails {
     private static final long serialVersionUID = 1L;
 
-    private Long id;
+    private final Long id;
 
-    private String username;
+    private final String username;
 
-    private String email;
+    private final String email;
 
     @JsonIgnore
-    private String password;
+    private final String password;
 
-    private Collection<? extends GrantedAuthority> authorities;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetailsImpl(Long id, String username, String email, String password,
+                           Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
 
 }
