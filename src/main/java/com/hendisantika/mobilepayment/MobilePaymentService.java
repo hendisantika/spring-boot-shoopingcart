7a2ccf4cbb19d4eb6ae4b3ceb50341f271c19e3a
Hendi Santika <hendisantika@gmail.com>
2021-10-30 06:15:54 +0700

    #62 - Add chargeMobilePayment in MobilePaymentService

--- src/main/java/com/hendisantika/mobilepayment/MobilePaymentService.java
+++ src/main/java/com/hendisantika/mobilepayment/MobilePaymentService.java
@@ -1,9 +1,14 @@
 package com.hendisantika.mobilepayment;
 
+import com.hendisantika.payload.response.CartResponse;
 import com.hendisantika.repository.CartRepository;
 import com.hendisantika.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +27,20 @@ public class MobilePaymentService {
 
     @Autowired
     private ItemService itemService;
+
+    public String chargeMobilePayment(int userID, List<CartResponse> cartResponseList) {
+        Date date = new Date();
+        long time = date.getTime();
+        Timestamp curentTime = new Timestamp(time);
+
+        cartRepository.updateCartPaymentSuccess(userID, curentTime);
+
+        //Reduce stock when customer paid
+        for (CartResponse cartResponse : cartResponseList) {
+            itemService.updateStockCustomer(cartResponse.getQty(), cartResponse.getItemId());
+        }
+
+        return "success";
+    }
+
 }
